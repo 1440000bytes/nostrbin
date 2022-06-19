324d9984e4a18cab4d20ebbc78825afd87a62000
/dev/fd0 <94559964+1440000bytes@users.noreply.github.com>
2022-06-19 18:39:59 +0530

    add headers

--- app.py
+++ app.py
@@ -14,7 +14,9 @@ def nostr(paste):
     url = "http://127.0.0.1:5000/create?paste="+paste
 
     payload={}
-    headers = {}
+    headers = {
+     'Content-Type': 'application/json'
+    }
 
     response = requests.request("GET", url, headers=headers, data=payload)
     eventid = json.loads(response.text)
