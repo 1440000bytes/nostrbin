fd2529db4a6e0a2c11acd63d2ed70561b7e3bc83
/dev/fd0 <94559964+1440000bytes@users.noreply.github.com>
2022-06-19 18:26:38 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import *
+import requests
+import json
+
+app = Flask(__name__)
+
+@app.route('/', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        return nostr(request.form['paste'])
+    return render_template('create.html')
+
+def nostr(paste):
+    url = "http://127.0.0.1:5000/create?paste="+paste
+
+    payload={}
+    headers = {}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    eventid = json.loads(response.text)
+
+    return eventid
+
+
+if __name__ == "__main__":
+    app.run('127.0.0.1', port=5555)
